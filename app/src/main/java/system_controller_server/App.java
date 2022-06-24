442c05dce30b761615efddf4d1a1c4460b23be97
klaz <giwaklaz@gmail.com>
2022-06-24 17:26:07 +0100

    initial commit

---
+++ app/src/main/java/system_controller_server/App.java
@@ -0,0 +1,28 @@
+package system_controller_server;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(App.class.getResource("/scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(App.class.getResource("/styles.css").toExternalForm());
+        
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
