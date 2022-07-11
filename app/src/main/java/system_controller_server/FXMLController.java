7f77433e02a3a68ff176be47982fdc28cf7f7d6f
klaz <giwaklaz@gmail.com>
2022-07-11 22:56:52 +0100

    displaying available port to user

--- app/src/main/java/system_controller_server/FXMLController.java
+++ app/src/main/java/system_controller_server/FXMLController.java
@@ -2,6 +2,7 @@ package system_controller_server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,15 +15,20 @@ import javafx.scene.control.Label;
 public class FXMLController implements Initializable {
 
     @FXML
-    private Label ipLabel, ipValue;
+    private Label ipLabel, ipValue, portLabel, portValue;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
             ipLabel.setText("IP Address: ");
+            portLabel.setText("Port: ");
+
             Socket socket = new Socket();
             socket.connect(new InetSocketAddress("google.com", 80));
             ipValue.setText(socket.getLocalAddress().toString().replace("/", ""));
+
+            ServerSocket server = new ServerSocket(0);
+            portValue.setText(String.valueOf(server.getLocalPort()));
         } catch (IOException ex) {
             Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
         }
